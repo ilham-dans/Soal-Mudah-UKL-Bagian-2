d4a8473ad7aaf43650f69840b5a349eed6c46f62
ilham-dans <muhammad_daniswara_34rpl@student.smktelkom-mlg.sch.id>
2025-11-04 23:36:14 +0700

    Add files via upload

---
+++ soalnumber1bagian2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class soalnumber1bagian2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+double bilangan; 
+System.out.println("masukan bilangan: "  );
+bilangan = input.nextDouble();
+
+if (bilangan %2 == 0) {
+    System.out.println("bilangan genap");}
+     else { System.out.println("bilangan ganjil");}
+
+}
+}
